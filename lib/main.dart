a735a5b3469b97fc626e7c201207652b52e88c3d
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-09 20:12:45 +0100

    chore: Update main.dart to initialize Firebase based on platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/firebase_options.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
@@ -7,9 +8,22 @@ import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyCZwyV6tLatb1lNv_iX6Xkh0nu_5QC-Rvo',
+        appId: '1:839507093658:web:e2d2d6d08422ab72909723',
+        messagingSenderId: '839507093658',
+        projectId: 'instagram-flutter-ad9b1',
+        storageBucket: 'instagram-flutter-ad9b1.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(const MainApp());
 }
 
