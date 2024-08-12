7081e8f4ce7823d3590a9b49efd347c296ae2d19
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-12 19:55:41 +0100

    feat: Add default post image and update post screen layout

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -10,10 +12,69 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.upload),
+    // return Center(
+    //   child: IconButton(
+    //     onPressed: () {},
+    //     icon: const Icon(Icons.upload),
+    //   ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+        title: const Text('Post to'),
+        centerTitle: false,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Post',
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(defaultPostImage),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Write a caption...',
+                    border: InputBorder.none,
+                  ),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                    aspectRatio: 487 / 451,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(defaultPostImage),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+          const Divider(),
+        ],
       ),
     );
   }
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -6,6 +6,9 @@ const webScreenSize = 600;
 const String defaultProfilePic =
     'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png';
 
+const String defaultPostImage =
+    'https://plus.unsplash.com/premium_photo-1720380988344-1fce88bc53a6?q=80&w=2148&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
 const homeScreenItems = [
   Center(
     child: Text('Home'),
