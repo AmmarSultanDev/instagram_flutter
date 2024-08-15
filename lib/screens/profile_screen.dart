db6037e315c52947b0f42d5253233998b863a734
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-15 18:13:44 +0100

    Refactor ProfileScreen to include user profile information and improve code structure

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,24 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/models/user.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/resources/auth_methods.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+import 'package:provider/provider.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatefulWidget {
+  ProfileScreen({super.key, this.user});
+
+  User? user;
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.user == null) {
+      widget.user = Provider.of<UserProvider>(context, listen: false).getUser;
+    }
+    //widget.user ??= Provider.of<UserProvider>(context).getUser;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        icon: const Icon(Icons.logout),
-        onPressed: () {
-          AuthMethods().logOut();
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-              builder: (context) => const LoginScreen(),
-            ),
-          );
-        },
+    final User? user = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: user?.uid == widget.user!.uid
+            ? Text(user!.username)
+            : Text(widget.user!.username),
+        actions: user?.uid == widget.user!.uid
+            ? [
+                IconButton(
+                    icon: const Icon(Icons.exit_to_app),
+                    onPressed: () {
+                      AuthMethods().logOut();
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                      );
+                    }),
+              ]
+            : [],
       ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage:
+                          Image.asset('assets/defaultProfileImage.png').image,
+                      radius: 40,
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          buildStateColumn(0, 'Posts'),
+                          buildStateColumn(0, 'Followers'),
+                          buildStateColumn(0, 'Following'),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Column buildStateColumn(int num, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          num.toString(),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4),
+          child: Text(
+            label.toString(),
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
